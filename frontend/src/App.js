3e0082b8e76991be1db825d7b207c160e23b5af0
robertbhanu <robertbhanuprasd0108@gmail.com>
2023-07-13 19:26:03 +0530

    ok

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <div className="bhanu">
+      kwdm
+      <h1>usdld</h1>
+   </div>
+   </>
+  );
+}
+
+export default App;
